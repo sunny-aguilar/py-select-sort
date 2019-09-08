25769ddf3993876ea36a5af6e78100c65ff9bd21
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-08 15:14:14 -0700

    ss

--- py-select-sort.py
+++ py-select-sort.py
@@ -1,7 +1,7 @@
 # select sort algorithm
 def findSmallest(arr):
     smallest = arr[0]
-    index = 0
+    smallest_index = 0
     for i in range(1 - len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
